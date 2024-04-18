9053e8595dcec7818264e745b5ec8af362367247
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-18 18:04:43 +0100

    Completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
 import Food from "./components/Food";
+import Category from "./components/Category";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Hero />
       <HeadlineCards />
       <Food />
+      <Category />
+      <Footer />
     </>
   );
 }
