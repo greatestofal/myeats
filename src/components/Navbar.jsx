e4c284aa8f332cc5645149a4965d9b8afd423287
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-18 15:56:30 +0100

    Added some components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState } from "react";
+
 import {
   AiFillTag,
   AiOutlineClose,
@@ -11,11 +13,12 @@ import { MdFavorite, MdHelp } from "react-icons/md";
 import { TbTruckDelivery } from "react-icons/tb";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       {/* Left Side */}
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
@@ -43,11 +46,22 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {/* Overlay */}
-      <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      {nav ? (
+        <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      ) : (
+        ""
+      )}
 
       {/* Side drawer menu */}
-      <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
+      <div
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
+      >
         <AiOutlineClose
+          onClick={() => setNav(!nav)}
           size={30}
           className="absolute right-4 top-4 cursor-pointer"
         />
